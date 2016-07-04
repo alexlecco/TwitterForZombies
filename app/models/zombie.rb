13d2b7f52116876925ac906e467b41b71cb42cde
alexlecco <alexlecco24@gmail.com>
2016-07-04 14:47:16 -0300

    [make_rotting callback was added]

--- app/models/zombie.rb
+++ app/models/zombie.rb
@@ -2,11 +2,15 @@ class Zombie < ActiveRecord::Base
   scope :rotting, where(rotting: true)
   scope :fresh, where("age < 20")
   scope :recent, order("created_at desc").limit(3)
-
 =begin
   Method chaining examples to create queries
-    Zombie.rotting.limit(5)
-    Zombie.recent.rotting
-    Zombie.recent.fresh.rotting
+  Zombie.rotting.limit(5)
+  Zombie.recent.rotting
+  Zombie.recent.fresh.rotting
 =end
-end
+
+  before_save :make_rotting
+
+  def make_rotting
+    self.rotting = true if age > 20
+  end
